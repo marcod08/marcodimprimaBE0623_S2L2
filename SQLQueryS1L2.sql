6a5257c38d7891f9b88f54b06b76431e4d5b71cd
Marco08 <147995679+marcod08@users.noreply.github.com>
2024-02-06 15:02:03 +0100

    Add files via upload

---
+++ SQLQueryS1L2.sql
@@ -0,0 +1,22 @@
+SELECT * FROM Products
+
+select * from Products
+where UnitsInStock > 39
+
+select * from Employees
+where City='London'
+
+select * from Orders
+order by freight desc
+
+select * from Orders
+where freight > 90 and freight < 200
+
+select * from Products
+where CategoryID=1
+
+select * from [Order Details]
+where discount > 0
+
+select * from Orders
+where customerid='bottm' and freight > 50
